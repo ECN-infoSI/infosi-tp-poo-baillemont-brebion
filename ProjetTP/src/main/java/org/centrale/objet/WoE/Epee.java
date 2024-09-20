6dbd993592e4df89b8c525ef1ab40a2b8e3a2b66
Matthieu Brebion <matt.brebion@orange.fr>
2024-09-20 16:28:13 +0200

    mise a jour de Epee

--- ProjetTP/src/main/java/org/centrale/objet/WoE/Epee.java
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/Epee.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.centrale.objet.WoE;
 
 /**
@@ -11,25 +7,49 @@ package org.centrale.objet.WoE;
 public class Epee extends Objet{
     
     private int ptAtt;
+    private int ptPar;
+    private String nom;
     
     public Epee(){
-        this.ptAtt = 10;
+        this.ptAtt = 20;
+        this.ptPar = 10;
+        this.nom = "Excalibur";
     }
     
-    public Epee(int pointsAttaque){
+    public Epee(int pointsAttaque, int pointsParade, String nom){
         this.ptAtt = pointsAttaque;
+        this.ptPar = pointsParade;
+        this.nom = nom;
     }
     
     public Epee(Epee e){
         this.ptAtt = e.getPtAtt();
+        
     }
 
     public int getPtAtt() {
         return ptAtt;
     }
+    
+    public int getPtPar() {
+        return ptPar;
+    }
+
+    public String getNom() {
+        return nom;
+    }
 
     public void setPtAtt(int ptAtt) {
         this.ptAtt = ptAtt;
     }
+
+    public void setPtPar(int ptPar) {
+        this.ptPar = ptPar;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    
     
 }
