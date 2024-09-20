950b560d81d05b50f4dce93040b9e251302ddd02
Matthieu Brebion <matt.brebion@orange.fr>
2024-09-20 16:10:48 +0200

    constructeur dans la classe Loup

--- ProjetTP/src/main/java/org/centrale/objet/WoE/Loup.java
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/Loup.java
@@ -1,13 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.centrale.objet.WoE;
 
 /**
  *
  * @author mattlerigolo
  */
-public class Loup {
+public class Loup extends Monstre {
+    public Loup(){
+        super();
+    }
     
+    public Loup(int ptVie, int degAtt, int ptPar, int pageAtt, int pagePar, Point2D pos){
+        super(ptVie, degAtt, ptPar, pageAtt, pagePar, pos);
+    }
+    
+    public Loup(Loup l){
+        super(l);
+    }
 }
