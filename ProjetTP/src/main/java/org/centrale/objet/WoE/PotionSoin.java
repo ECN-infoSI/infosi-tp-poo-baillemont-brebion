757c874077772c9454d1f9ae179a6db155951d10
Matthieu Brebion <matt.brebion@orange.fr>
2024-09-23 14:35:43 +0200

    ajout des attributs positions et consumed dans PotionSoin

--- ProjetTP/src/main/java/org/centrale/objet/WoE/PotionSoin.java
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/PotionSoin.java
@@ -11,25 +11,51 @@ package org.centrale.objet.WoE;
 public class PotionSoin extends Objet {
     
     private int ptSoin;
+    private Point2D pos;
+    private boolean consumed;
     
     public PotionSoin(){
         this.ptSoin = 10;
+        this.pos = new Point2D();
+        this.consumed = false;
     }
     
-    public PotionSoin(int pointsSoin){
+    public PotionSoin(int pointsSoin, Point2D pos){
         this.ptSoin = pointsSoin;
+        this.pos = new Point2D(pos);
+        this.consumed = false; // la potion est forcément pleine quand on la crée
     }
     
     public PotionSoin(PotionSoin p){
         this.ptSoin = p.getPtSoin();
+        this.pos = new Point2D(p.getPos());
+        this.consumed = p.isConsumed();
     }
 
     public int getPtSoin() {
         return ptSoin;
     }
 
+    public Point2D getPos() {
+        return pos;
+    }
+
+    public boolean isConsumed() {
+        return consumed;
+    }
+
     public void setPtSoin(int ptSoin) {
         this.ptSoin = ptSoin;
     }
+
+    public void setPos(Point2D pos) {
+        this.pos = pos;
+    }
+
+    public void setIsConsumed(boolean consumed) {
+        this.consumed = consumed;
+    }
+    
+    
     
 }
