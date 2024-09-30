5c7ffd9f5360bdfcafc11046bc38a921263e4141
momo838 <morgane0044@sfr.fr>
2024-09-30 15:24:53 +0200

    World_linkedList

--- ProjetTP/src/main/java/org/centrale/objet/WoE/World_linkedList.java
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/World_linkedList.java
@@ -1,6 +1,8 @@
 package org.centrale.objet.WoE;
+
 import java.util.Random;
-import java.util.function.Predicate;
+import java.util.LinkedList;
+
 
 /**
  * Création d'un monde WoE comprenant un archer, un paysan et un lapin. 
@@ -9,95 +11,188 @@ import java.util.function.Predicate;
  * @author morga
  * 
  */
-public class World_linkedList {
-    /** 
-     * Une instance d'Archer, robin.
-     * @see Archer
+public class World_linkedList{
+    
+    /** Linked List contenant les personnages du Monde.
+     * 
      */
-    public Archer robin;
-    
-    /** 
-     * Une instance d'Archer, guillaumeT.
-     * @see Archer
+    public LinkedList<Personnage> personnages = new LinkedList<>();
+    /** Linked List contenant les monstres du Monde.
+     * 
      */
-    public Archer guillaumeT;
+    public LinkedList<Monstre> monstres = new LinkedList<>();
     
-    /** 
-     * Une instance de Paysan, peon.
-     * @see Paysan
+    /** Linked List contenant les objets du Monde.
+     * 
      */
-    public Paysan peon;
+    public LinkedList<Objet> objets = new LinkedList<>();
     
-    /** 
-     * Une instance de Lapin, bugs1.
-     * @see Lapin
-     */
-    public Lapin bugs1;
-    
-    /** 
-     * Une instance de Lapin, bugs2.
-     * @see Lapin
-     */
-    public Lapin bugs2;
-    
-    /** 
-     * Une instance de Guerrier, grosBill
-     * @see Guerrier
-     */
-    public Guerrier grosBill;
-    
-    
-    /** 
-     * Une instance de Loup, wolfie
-     * @see Loup
-     */
-    public Loup wolfie;
-    
-    
-    
-    
-     /**
+    /**
      * Constructeur par défaut.
      */
     public World_linkedList(){
-       
-        this.robin = new Archer();
-        this.peon = new Paysan();
-        this.bugs1 = new Lapin();
-        this.bugs2 = new Lapin();
-        this.guillaumeT = new Archer();
-        this.grosBill = new Guerrier();
-        this.wolfie = new Loup();
+        this.personnages = new LinkedList<Personnage>();
+        this.monstres = new LinkedList<Monstre>();
+        this.objets = new LinkedList<Objet>();
     };
-    
     
     /**
      * Constructeur.
-     * @param a1
-     *          Archer 1
-     * @param p
-     *          Paysan
-     * @param l1
-     *          Lapin 1
-     * @param l2
-     *          Lapin 2
-     * @param a2
-     *          Archer 2
-     * @param g
-     *          Guerrier
-     * @param l3
-     *          Loup
+     * @param persos
+     * Liste de Personnage
      * 
+     * @param monstres
+     * Liste de monstres
+     * 
+     * @param objets
+     * Liste d'objets
      */
-    public World_linkedList(Archer a1, Paysan p, Lapin l1, Lapin l2, Archer a2, Guerrier g, Loup l3){
-        this.robin = a1;
-        this.peon = p;
-        this.bugs1 = l1;
-        this.bugs2 = l2;
-        this.guillaumeT = a2;
-        this.grosBill = g;
-        this.wolfie = l3;
+    public World_linkedList(LinkedList<Personnage> persos, LinkedList<Monstre> monstres, LinkedList<Objet> objets){
+        this.personnages = new LinkedList<Personnage>(persos);
+        this.monstres = new LinkedList<Monstre>(monstres);
+        this.objets = new LinkedList<Objet>(objets);
     }
+
+    public LinkedList<Personnage> getPersonnages() {
+        return personnages;
+    }
+
+    public void setPersonnages(LinkedList<Personnage> personnages) {
+        this.personnages = personnages;
+    }
+
+    public LinkedList<Monstre> getMonstres() {
+        return monstres;
+    }
+
+    public void setMonstres(LinkedList<Monstre> monstres) {
+        this.monstres = monstres;
+    }
+
+    public LinkedList<Objet> getObjets() {
+        return objets;
+    }
+
+    public void setObjets(LinkedList<Objet> objets) {
+        this.objets = objets;
+    }
+    
+    /**
+     * Ajouter un personnage à notre monde
+     * @param perso 
+     * Personnage à ajouter
+     */
+    public void addPersonnage(Personnage perso){
+        this.getPersonnages().add(perso);
+    }
+    
+    /**
+     * Ajouter un monstre au monde
+     * @param monstre 
+     */
+    public void addMonstre(Monstre monstre){
+        this.getMonstres().add(monstre);
+    }
+    
+    public void addObjet(Objet objet){
+        this.getObjets().add(objet);
+    }
+    
+   /**
+     * Constructeur.
+     * @param nbArchers
+     *          Nombre d'archers.
+     * @param nbPaysans
+     *          Nombre de paysans.
+     * @param nbGuerriers
+     *          Nombre de guerriers.
+     * @param nbLoups
+     *          Nombre de loups.
+     * @param nbLapins
+     *          Nombre de lapins.
+     * @param nbPotionSoins
+     *          Nombre de potions de soin.
+     * @param nbEpees
+     *          Nombre d'épées.
+     * 
+     */ 
+    public World_linkedList(int nbArchers, int nbPaysans, int nbGuerriers, int nbLoups, int nbLapins, int nbPotionSoins, int nbEpees){
+
+        // Crée les archers et les ajoute à la liste
+        for (int i = 0; i < nbArchers; i++) {
+            Archer archer = new Archer();
+            archer.setNom("Archer " + (i + 1));
+            listePersonnages.add(archer);
+        }
+
+        // Crée les guerriers et les ajoute à la liste
+        for (int i = 0; i < nbGuerriers; i++) {
+            Guerrier guerrier = new Guerrier();
+            guerrier.setNom("Guerrier " + (i + 1));
+            listePersonnages.add(guerrier);
+        }
+        
+        // Crée les paysans et les ajoute à la liste
+        for (int i = 0; i < nbPaysans; i++) {
+            Paysan paysan = new Paysan();
+            paysan.setNom("Paysan " + (i + 1));
+            listePersonnages.add(paysan);
+        }
+        
+
+        // Crée les loups et les ajoute à la liste
+        for (int i = 0; i < nbLoups; i++) {
+            Loup loup = new Loup();
+            listeMonstres.add(loup);
+        }
+
+        // Crée les lapins et les ajoute à la liste
+        for (int i = 0; i < nbLapins; i++) {
+            Lapin lapin = new Lapin();
+            listeMonstres.add(lapin);
+        }
+        
+        // Crée les Potions de Soin et les ajoute à la liste
+        for (int i = 0; i < nbPotionSoins; i++) {
+            PotionSoin potion = new PotionSoin();
+            listeObjets.add(potion);
+        }
+
+        // Crée les Epees et les ajoute à la liste
+        for (int i = 0; i < nbEpees; i++) {
+            Epee epee = new Epee();
+            listeObjets.add(epee);
+        }
+   
+    };
+
+    
+     /**
+     * Constructeur par défaut, crée un nombre alétoire de personnages, monstres et objets.
+     */
+    public World_linkedList(){
+       // Génère un nombre aléatoire de Personnages
+        Random random = new Random();
+        int nbArchers = random.nextInt();
+        int nbGuerriers = random.nextInt();
+        int nbPaysans = random.nextInt();
+        
+        // Génère un nombre aléatoire de Monstres
+        int nbLoups = random.nextInt();
+        int nbLapins = random.nextInt();
+
+        // Génère un nombre aléatoire d'Objets.
+        int nbPotionSoins = random.nextInt();
+        int nbEpees = random.nextInt();
+
+        Wornew World_linkedList(nbArchers, nbPaysans, nbGuerriers, nbLoups, nbLapins, nbPotionSoins, nbEpees);
+   
+    };
+    
+    
+    
+
+    
     /** 
     * Création des positions aléatoires des objets du monde. 
     * Deux objets ne peuvent pas être à la même position.
