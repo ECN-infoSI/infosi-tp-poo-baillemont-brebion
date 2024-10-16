8d56a769889fb2f5a9fb02abb23a8c12f9c9b24e
Matthieu Brebion <matt.brebion@orange.fr>
2024-10-16 13:50:21 +0200

    modifs de con

--- ProjetTP/src/main/java/org/centrale/objet/WoE/ProjetTP.java
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/ProjetTP.java
@@ -10,11 +10,6 @@ public class ProjetTP {
     public static void main(String[] args) {
         World_arrayList monde = new World_arrayList(5,5,false);
         GameLoop Game = new GameLoop(monde);
-        monde.creaMondeAlea(1,1,1,1,1,20,20,20,5);
-        monde.getPersonnages().get(0).setNom("Ivan");
-        monde.getPersonnages().get(0).setPtVie(1);
-        monde.getPersonnages().get(1).setNom("Dmitri");
-        monde.getPersonnages().get(2).setNom("Aliocha");
         
         Game.startGame();
     }
